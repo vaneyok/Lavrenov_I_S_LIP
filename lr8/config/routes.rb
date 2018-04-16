d3109c762ba397bca488adf72c0fb568d496f783
LIS <34397738+vaneyok@users.noreply.github.com>
2018-04-16 17:40:35 +0300

    Add files via upload

---
+++ lr8/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'lr8#input'
+
+  get 'lr8/input'
+
+  get 'lr8/output'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
